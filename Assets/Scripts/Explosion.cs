204e30c3cc19029f77735e6d7e3197cc32767c1b
LuckyDenzel <luckydenzel1@gmail.com>
2025-08-25 12:18:59 +0200

    [ADDED] an explosive which explodes on collision. [FIXED] Refined some things to match better with a 2D game.

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Explosion : MonoBehaviour {
+
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D eplosiveRb;
+    [SerializeField] private ParticleSystem explosionParticleSystem;
+
+    [Header("Values")]
+    [SerializeField] private float startingExplosionRadius = 3f;
+    [SerializeField] private float startingExplosionDamage = 10f;
+
+    [SerializeField] private float explosiveLifetimeDuration = 14f;
+    [SerializeField] private float explosionLifetimeDuration = 7f;
+
+    private float currentExplosionRadius;
+    private float currentExplosionDamage;
+
+    private bool hasExploded;
+
+    private void Start() {
+        explosionParticleSystem.gameObject.SetActive(false);
+
+        currentExplosionRadius = startingExplosionRadius;
+        currentExplosionDamage = startingExplosionDamage;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (!hasExploded) {
+            Explode();
+
+            hasExploded = true;
+        }
+    }
+
+    private void Explode() {
+        Collider2D[] objectColliders = Physics2D.OverlapCircleAll(transform.position, currentExplosionRadius);
+
+        if (objectColliders != null) {
+            foreach (Collider2D obj in objectColliders) {
+                Rigidbody2D objectRb = obj.attachedRigidbody;
+
+                if (objectRb != null) {
+                    Vector2 forceDirection = objectRb.position - (Vector2)transform.position;
+
+                    objectRb.AddForce(forceDirection, ForceMode2D.Impulse);
+                }
+            }
+        }
+
+        explosionParticleSystem.gameObject.SetActive(true);
+        explosionParticleSystem.Play();
+
+        // Handle deletion of this object
+        Destroy(gameObject, explosiveLifetimeDuration);
+
+        // Handle deletion of the explosion particle system
+        StartCoroutine(HandleExplosionLifetime());
+    }
+
+    private IEnumerator HandleExplosionLifetime() {
+        // Decouple the particle system so that it doesn't rol with the object
+        explosionParticleSystem.transform.SetParent(null);
+
+        yield return new WaitForSeconds(explosionLifetimeDuration);
+
+        // For now destroy the explosion, later maybe use a pooler
+        Destroy(explosionParticleSystem.gameObject);
+    }
+}
