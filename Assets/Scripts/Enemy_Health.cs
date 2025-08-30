02a332ec90e54ab55c53c4d7642a71d9caf42ecc
LuckyDenzel <luckydenzel1@gmail.com>
2025-08-30 15:12:17 +0200

    [ADDED] An enemy health bar UI.

--- Assets/Scripts/Enemy_Health.cs
+++ Assets/Scripts/Enemy_Health.cs
@@ -1,7 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Enemy_Health : MonoBehaviour, IHealth {
 
+
+    public event EventHandler<OnHealthChangedEventArgs> OnHealthChanged;
+    public class OnHealthChangedEventArgs : EventArgs {
+        public int newHealth;
+
+        public OnHealthChangedEventArgs(int newHealth) {
+            this.newHealth = newHealth;
+        }
+    }
 
     [SerializeField] private int maxHealth = 100;
 
@@ -16,6 +26,8 @@ public class Enemy_Health : MonoBehaviour, IHealth {
     public void AddHealth(int amount) {
         if (CurrentHealth > 0 && CurrentHealth + amount <= maxHealth) {
             CurrentHealth += amount;
+
+            OnHealthChanged?.Invoke(this, new OnHealthChangedEventArgs(CurrentHealth));
         } else {
             Debug.LogError("You're trying to add health to an enemy, but the enemy is already dead. This should never happen!", this);
         }
@@ -27,6 +39,8 @@ public class Enemy_Health : MonoBehaviour, IHealth {
         if (CurrentHealth <= 0) {
             Die();
         }
+
+        OnHealthChanged?.Invoke(this, new OnHealthChangedEventArgs(CurrentHealth));
     }
 
     private void Die() {
---
+++ Assets/Scripts/UI/EnemyHealthUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthUI : MonoBehaviour {
+
+
+    [SerializeField] private Enemy_Health enemyHealth;
+    [SerializeField] private Image enemyHealthImage;
+    [SerializeField] private Image enemyHealthBackgroundImage;
+
+
+    private void Start() {
+        enemyHealth.OnHealthChanged += EnemyHealth_OnHealthChanged;
+
+        enemyHealthImage.fillAmount = 100;
+        enemyHealthImage.gameObject.SetActive(false);
+        enemyHealthBackgroundImage.gameObject.SetActive(false);
+    }
+
+    private void EnemyHealth_OnHealthChanged(object sender, Enemy_Health.OnHealthChangedEventArgs e) {
+        float maxValue = 100;
+        float normalizedHealth = e.newHealth / maxValue;
+
+        enemyHealthImage.fillAmount = normalizedHealth;
+
+        enemyHealthImage.gameObject.SetActive(true);
+        enemyHealthBackgroundImage.gameObject.SetActive(true);
+
+        Debug.Log(e.newHealth + enemyHealthImage.fillAmount);
+    }
+}
